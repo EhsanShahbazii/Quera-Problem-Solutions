88bea159ca6de1014e564d37462dacb4b5113c22
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-09-28 13:25:24 +0330

    Create YakhDarChiS1.java
    solve

---
+++ Tournonment/YakhDarChiS1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+          int temperature = scanner.nextInt();
+
+          if (temperature > 100)
+              System.out.print("Steam");
+          else if (temperature < 0)
+              System.out.print("Ice");
+          else
+              System.out.print("Water");
+        }
+    }
+}
+// <Ctrl+Z>
