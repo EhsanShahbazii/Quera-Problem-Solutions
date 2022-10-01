8ac86bac93d0c4458713c187080a190d204091c7
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-02 00:29:38 +0330

    Create HansFeriHaS1.java
    solve

---
+++ Tournonment/HansFeriHaS1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            String a = scanner.next();
+            String b = scanner.next();
+            String c = scanner.next();
+            String d = scanner.next();
+
+            if (a.equals(d) || a.equals(b) || b.equals(c) || c.equals(d))
+                System.out.print("YES");
+            else
+                System.out.print("NO");
+        }
+    }
+}
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            String a = scanner.next();
+            String b = scanner.next();
+            String c = scanner.next();
+            String d = scanner.next();
+
+            if (a.equals(d) || a.equals(b) || b.equals(c) || c.equals(d))
+                System.out.print("YES");
+            else
+                System.out.print("NO");
+        }
+    }
+}
+
+// </Ctrl+Z>
