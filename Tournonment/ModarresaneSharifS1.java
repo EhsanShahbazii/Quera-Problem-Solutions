f4db61f31ed6f905fe238b44055aedbed1ec1a39
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-03 00:11:52 +0330

    Create ModarresaneSharifS1.java
    solve

---
+++ Tournonment/ModarresaneSharifS1.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(System.in));
+
+            String name = reader.readLine();
+            System.out.print("quera!");
+
+    }
+}
+
+// </Ctrl+Z>
