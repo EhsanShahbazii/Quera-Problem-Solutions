e5685b2ea7119002eaed6dc1c65324ed68fb4329
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-03 00:02:23 +0330

    Create NomayeshgaheMajaziS1.java
    solve

---
+++ Tournonment/NomayeshgaheMajaziS1.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            int n = scanner.nextInt();
+
+            switch (n) {
+                case 0:
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 1:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1..............#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 2:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 3:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3..............#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 4:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3.......ghorfe4#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 5:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3.......ghorfe4#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe5..............#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 6:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3.......ghorfe4#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe5.......ghorfe6#");
+                    System.out.println("########.......########");
+                    System.out.println("#.....................#");
+                    System.out.println("#######################");
+                    break;
+                case 7:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3.......ghorfe4#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe5.......ghorfe6#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe7..............#");
+                    System.out.println("#######################");
+                    break;
+                case 8:
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe1.......ghorfe2#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe3.......ghorfe4#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe5.......ghorfe6#");
+                    System.out.println("########.......########");
+                    System.out.println("#ghorfe7.......ghorfe8#");
+                    System.out.println("#######################");
+                    break;
+
+            }
+
+        }
+    }
+
+
+
+}
