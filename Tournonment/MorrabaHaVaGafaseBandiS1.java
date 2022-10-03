7029460f40632d35f6c754924ef65854c6ab2b34
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-04 00:48:03 +0330

    Create MorrabaHaVaGafaseBandiS1.java
    solve

---
+++ Tournonment/MorrabaHaVaGafaseBandiS1.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<Member> arrayList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            Member member = new Member(a,b);
+            arrayList.add(member);
+        }
+        int ans = n + 1;
+        for (int i = 0; i < arrayList.size(); i++) {
+            for (int j = i + 1; j < arrayList.size(); j++) {
+                if (arrayList.get(i).a != arrayList.get(j).a){
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+    static class Member{
+        int a,b;
+
+        public Member(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
+
+// </Ctrl+Z>
