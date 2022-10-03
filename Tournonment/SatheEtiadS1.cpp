72c60d0b9b070d68907e065ffa304e5ce838dc9e
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-04 00:40:39 +0330

    Update SatheEtiadS1.cpp

--- Tournonment/SatheEtiadS1.cpp
+++ Tournonment/SatheEtiadS1.cpp
@@ -1,3 +1,7 @@
+#include<bits/stdc++.h>
+#define watch(x) cout<<(#x)<<" is "<<(x)<<"\n"
+#define print(x) cout<<x<<"\n"
+#define For(i,x,y) for(i = x; i < y; i++)
 typedef std::vector<int> vi;
 typedef std::vector<std::vector<int>> vvi;
 typedef long long ll;
