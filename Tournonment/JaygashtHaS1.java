2af25803a15db0bc320e77cba7bad3399d7bb3a5
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-06 00:37:16 +0330

    Create JaygashtHaS1.java
    solve

---
+++ Tournonment/JaygashtHaS1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main {
+  
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+      
+        int n = scanner.nextInt();
+      
+        int arr[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int x = scanner.nextInt();
+          
+            for (int j = 1; j < arr.length; j++) {
+              
+                if (arr[j] == 0){
+                    int k = 0, kk = j;
+                    while (k < x){
+                        if (arr[kk] == 0) {
+                            k++;
+                        }
+                        kk++;
+                    }
+                    if (k == x) {
+                        while (arr[kk] != 0)
+                            kk++;
+                        arr[kk] = i;
+                        break;
+                    }
+                }
+            }
+          
+        }
+        for (int i = 1; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println(arr[arr.length - 1]);
+    }
+}
+
+// </Ctrl+Z>
