63a1f54957de16ab818f78e20902c0b2c3b7bdfd
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-06 00:35:01 +0330

    Create HaddeaksarYaleGraphS1.java
    solve

---
+++ Tournonment/HaddeaksarYaleGraphS1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            long res = ((long) n *(n-1))/2;
+            System.out.println(res);
+        }
+    }
+}
+
+// </Ctrl+Z>
