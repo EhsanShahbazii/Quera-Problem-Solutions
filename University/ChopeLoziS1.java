3a903118151716b2c33abb1c84736dd9c7868ea6
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-10-12 00:54:39 +0330

    Create ChopeLoziS1.java
    solve

---
+++ University/ChopeLoziS1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        int n, i, j, space = 1;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        n++;
+        space = n - 1;
+        for (j = 1; j <= n; j++) {
+            for (i = 1; i <= space; i++) {
+                System.out.print(" ");
+            }
+            space--;
+            for (i = 1; i <= 2 * j - 1; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        space = 1;
+        for (j = 1; j <= n - 1; j++) {
+            for (i = 1; i <= space; i++) {
+                System.out.print(" ");
+            }
+            space++;
+            for (i = 1; i <= 2 * (n - j) - 1; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+// </Ctrl+Z>
