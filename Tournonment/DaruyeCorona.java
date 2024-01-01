c3a4a9de8244abb7db04703486e854cf7f7161b5
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-01 15:06:23 +0330

    Create DaruyeCorona.java

---
+++ Tournonment/DaruyeCorona.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        if (a - b > c - d) {
+            System.out.println("Shekarestan");
+        } else if (a - b == c -d) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Namakestan");
+        }
+    }
+}
