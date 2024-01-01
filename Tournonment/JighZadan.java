55acb9fc1d579b5dd20e714bacc1f59eb4df7a32
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-01 19:30:33 +0330

    Create JighZadan.java

---
+++ Tournonment/JighZadan.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+         if (a == 1) {
+             System.out.println(2);
+         } else {
+             System.out.println(3);
+         }
+    }
+}
