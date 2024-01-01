c361dbddfbbb655e13f1c602ff39670e1b1447a4
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-01 19:38:11 +0330

    Create Baalin.py

---
+++ Tournonment/Baalin.py
@@ -0,0 +1,8 @@
+def calculate_floor(string):
+    now = 0
+    for i, c in enumerate(string):
+        if (c == "U"):
+            now = now + 1
+        else:
+            now = now - 1
+    return now
