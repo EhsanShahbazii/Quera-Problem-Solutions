39b8225ba5837ae22bba2d935ebd7acd81e6260c
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-01 14:55:51 +0330

    Create SiSharp.java

---
+++ Tournonment/SiSharp.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        for (int i = 0; i < a; i++) {
+            System.out.print("#");
+        }
+    }
+}
