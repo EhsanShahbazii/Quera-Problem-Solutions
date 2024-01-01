16e391a8d29ba03727b9f811b63116a656616939
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-01 14:54:01 +0330

    Create ModiriateZamaneNima.java

---
+++ Tournonment/ModiriateZamaneNima.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println(24 - (a + b - c));
+    }
+}
