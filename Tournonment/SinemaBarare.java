39b233aada503cd20d67b8bd93e74bcd8bf0738a
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-06 20:21:21 +0330

    Create SinemaBarare.java

---
+++ Tournonment/SinemaBarare.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+
+        System.out.println(Math.min(a,b));
+    }
+}
