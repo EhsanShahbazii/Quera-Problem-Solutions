231b1cbe9a0b604ca6d1e8f17bdbadf809e89c72
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-12 11:44:52 +0330

    Create TarikheTavallodS1.py

---
+++ University/TarikheTavallodS1.py
@@ -0,0 +1,7 @@
+def main():
+    number = input()
+    print("saal: {}{}".format(number[0], number[1]))
+    print("maah: {}{}".format(number[2], number[3]))
+
+if __name__ == "__main__":
+    main()
