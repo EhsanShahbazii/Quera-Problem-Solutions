25bca1dae517adb281d66af258cd9cf0fddfe797
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-14 20:42:41 +0330

    Create TarikheTavallodS1.cs

---
+++ University/TarikheTavallodS1.cs
@@ -0,0 +1,14 @@
+using System;
+using System.IO;
+public class Main
+{
+    public static void Main(string[] args)
+    {
+        try (public const  scanner =  "Inputs";)
+        {
+            var number = Console.ReadLine();
+            Console.WriteLine("saal:" + number[0].ToString() + number[1].ToString());
+            Console.WriteLine("maah:" + number[2].ToString() + number[3].ToString());
+        }
+    }
+}
