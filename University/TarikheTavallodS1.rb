4a7ddeffdba6c707882debd8dc774a5edd69aa56
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-15 23:18:42 +0330

    Create TarikheTavallodS1.rb

---
+++ University/TarikheTavallodS1.rb
@@ -0,0 +1,11 @@
+class Main
+    def self.main()
+        try ()
+        {
+            number = gets.chomp
+            print("saal:" + number[0].to_s + number[1].to_s,"\n")
+            print("maah:" + number[2].to_s + number[3].to_s,"\n")
+        }
+    end
+end
+Main.main()
