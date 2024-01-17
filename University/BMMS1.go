73495163d09b32ea671bf99627f586a0017e5966
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-17 12:54:18 +0330

    Create BMMS1.go

---
+++ University/BMMS1.go
@@ -0,0 +1,34 @@
+package main
+import "os"
+import "fmt"
+
+func get_input[T any]() T {
+	var i T
+	fmt.Scan(&i)
+	return i
+}
+type Main struct {
+}
+func Main_1() * Main {
+    var this * Main =  &Main{};
+    return this;
+}
+func (this *Main) main_1(args []string) {
+    try () {
+        var  n  int64 = get_input[int64]();
+        var  m  int64 = get_input[int64]();
+        fmt.Print(this.gcd(n, m));
+    }
+}
+func (this *Main) gcd( p  int64,  q  int64)int64 {
+    if (q == 0) {
+        return p;
+    } else {
+        return this.gcd(q, p % q);
+    }
+}
+
+func main(){
+	var task = Main{};
+	task.main_1(os.Args);
+}
