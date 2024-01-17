13a39e56effc0dad7581647a61ace005682c3818
Ehsan <ehsan.shahbazicode@gmail.com>
2024-01-17 12:49:48 +0330

    Create AlphaGonturusS1.rb

---
+++ University/AlphaGonturusS1.rb
@@ -0,0 +1,12 @@
+class Main
+    def self.main()
+        try ()
+        {
+            n = gets.chomp
+            b = gets.chomp
+            result = Integer.Integer(n.join(""),10),b.to_s
+            print(result.toUpperCase())
+        }
+    end
+end
+Main.main()
